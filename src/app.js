3af8bfc3d95b5d415813a894fa00c99417f0ae2e
Fahad Javed <fahadjaved203@gmail.com>
2026-02-21 04:11:41 +0500

    Added code for npm run Buid video calling in node

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ import morgan from "morgan";
 import logger from "./logger.js";
 import enrollmentRoutes from "./routes/enrollmentRoutes.js";
 import testRoutes from "./routes/test.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ✅ routes
+app.use("/api/enroll", enrollmentRoutes);
+app.use("/api/test", testRoutes);
 app.get("/api/health", (req, res) => {
   res.json({ status: "OK", message: "Backend is running" });
 });
@@ -25,9 +31,25 @@ app.use((req, res, next) => {
   next();
 });
 
-// ✅ routes
-app.use("/api/enroll", enrollmentRoutes);
-app.use("/api/test", testRoutes);
+// ES Modules require this to get __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log("Directory Name ="+__dirname)
+console.log("File Name ="+__filename)
+
+// ✅ Correct React build path (backend ke bahar)
+const frontendBuildPath = "C:/Users/Fahad Javed/Desktop/iqra-quran-academy/frontend/iqra-quran-academy-react/dist";
+
+console.log("Front end build path ="+frontendBuildPath)
+
+// Serve React build
+app.use(express.static(frontendBuildPath));
+
+// Express 5 SPA wildcard fix
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(frontendBuildPath, "index.html"));
+});
+
 
 
 
